1791eaa778947b3388061f31e422d45e7f543bff
tg1991 <36793219+tg1991@users.noreply.github.com>
2020-07-01 15:43:48 +0100

    testing draft
    rough draft of test layouts and what they will be looking for

--- MuscleFoodTask/Specs/VendingMachneTests.js
+++ MuscleFoodTask/Specs/VendingMachneTests.js
@@ -38,12 +38,69 @@ describe('Vending Machine Test', function () {
                     //code here
                 });
 
+            it('if any other coins are added then they will be rejected',
+                function () {
+                    //code here
+                });
+
+        });
+
+    describe('Select Product',
+        function () {
+            it('select product cola where exact money used and display shows THANK YOU, then ' +
+                'display shows INSERT COIN and current amount equals $0.00',
+                function () {
+                    //code here
+                });
+
+            it('select product cola where not enough money is added display shows INSERT COINS',
+                function () {
+                    //code here
+                });
+
+            it('select product chips where exact money used and display shows THANK YOU, then ' +
+                'display shows INSERT COIN and current amount equals $0.00',
+                function () {
+                    //code here
+                });
+
+            it('select product chips where not enough money is added display shows INSERT COINS',
+                function () {
+                    //code here
+                });
+
+            it('select product candy where exact money used and display shows THANK YOU, then ' +
+                'display shows INSERT COIN and current amount equals $0.00',
+                function () {
+                    //code here
+                });
+
+            it('select product candy where not enough money is added display shows INSERT COINS',
+                function () {
+                    //code here
+                });
+
         });
 
 
+    describe('Make Change',
+        function () {
+            it('select product candy where too much money is added display shows THANK YOU, then' +
+                'remaining amount placed in coin return and display shows INSERT COIN' +
+                'and current amount equals $0.00',
+                function () {
+                    //code here
+                });
+        });
 
-
-
+    describe('Return Coins',
+        function () {
+            it('coins are inserted and return coins button is pressed, coins returned and' +
+                'display shows INSERT COINS',
+                function () {
+                    //code here
+                });
+        });
 
 
 });
