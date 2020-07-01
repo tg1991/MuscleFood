6bbe7ff876ca4d8b083d7f128da65a35fbc6f9be
tg1991 <36793219+tg1991@users.noreply.github.com>
2020-07-01 14:03:39 +0100

    Accept Coins Feature Plan
    Plan for first AC which is Accept Coins

--- MuscleFoodTask/Specs/VendingMachneTests.js
+++ MuscleFoodTask/Specs/VendingMachneTests.js
@@ -1,3 +1,49 @@
 ﻿/// <reference path="..\Scripts/jasmine/jasmine.js" />
 /// <reference path="..\Scripts/VendingMachine.js" />
 
+describe('Vending Machine Test', function () {
+
+
+
+    //tests for first task.
+
+    describe('page loading',
+        function () {
+            it('on page load machine displays INSERT COINS',
+                function () {
+                    //code here
+                });
+        });
+
+
+    describe('Adding Coins',
+        function () {
+            it('adding in a nickel coin and display shows $0.05',
+                function () {
+                    //code here
+                });
+
+            it('adding in a dime coin and display shows $0.10',
+                function () {
+                    //code here
+                });
+
+            it('adding in a quarter coin and display shows $0.25',
+                function () {
+                    //code here
+                });
+
+            it('adding in a penny coin and display shows INSERT COIN and coin return equals $0.01',
+                function () {
+                    //code here
+                });
+
+        });
+
+
+
+
+
+
+
+});
